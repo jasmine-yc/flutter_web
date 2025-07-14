3a7cfa1dc7b6ba5bdbaa802b77b43426b432ea76
jasmine-yc <dnrysrhr@gmail.com>
2025-07-14 20:45:35 +0800

    Deploy Flutter Web with correct base href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2694531619"
+    serviceWorkerVersion: "866054349"
   }
 });
