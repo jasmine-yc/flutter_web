a5230c45760dd5b290fc111aa445d04b42ec1a9a
jasmine-yc <dnrysrhr@gmail.com>
2025-07-16 21:34:52 +0800

    Deploy new build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3179742459"
+    serviceWorkerVersion: "2021794859"
   }
 });
