49d366f6cce12bb457dd4920f0c2161d2a9544f6
jasmine-yc <dnrysrhr@gmail.com>
2025-07-17 14:14:51 +0800

    feat: update project with latest web build

---
+++ lib/pages/lyrics_furigana_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../models/lyrics.dart';
+
+class LyricsFuriganaPage extends StatelessWidget {
+  final Lyrics song;
+
+  const LyricsFuriganaPage({super.key, required this.song});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${song.title} - 空耳'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 146, 170, 194),
+      ),
+      backgroundColor: Color.fromARGB(255, 255, 249, 242),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(song.furigana, style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
---
+++ lib/pages/lyrics_translation_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../models/lyrics.dart';
+
+class LyricsTranslationPage extends StatelessWidget {
+  final Lyrics song;
+
+  const LyricsTranslationPage({super.key, required this.song});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${song.title} - 中譯'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 146, 170, 194),
+      ),
+      backgroundColor: Color.fromARGB(255, 255, 249, 242),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(song.translation, style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
